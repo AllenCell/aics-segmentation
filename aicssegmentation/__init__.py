963fd96453422e8fa4b96c9a4db3fcbd249cbd38
Jianxu Chen <jianxuc@n204.corp.alleninst.org>
2020-11-24 18:42:02 -0800

    Initial commit

---
+++ aicssegmentation/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for aics-segmentation."""
+
+__author__ = "Jianxu Chen"
+__email__ = "jianxuc@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.20"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
