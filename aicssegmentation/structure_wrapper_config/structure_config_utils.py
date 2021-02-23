02e752bd1dbcfe2f6ac691d7234196d3abc4e371
Jianxu Chen <jxchen.ustc@gmail.com>
2021-02-22 17:31:03 -0800

    populuate sec61 workflow, and loading function

--- aicssegmentation/structure_wrapper_config/structure_config_utils.py
+++ aicssegmentation/structure_wrapper_config/structure_config_utils.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path, PurePosixPath
 
 
@@ -8,3 +9,12 @@ def get_all_workflows_avail_in_json(return_list: bool = False):
 
     if return_list:
         return all_names
+
+
+def load_workflow_config(workflow_name: str):
+
+    json_path = Path(__file__).parent / f"conf_{workflow_name}.json"
+    with open(json_path, "r") as read_file:
+        cfg = json.load(read_file)
+
+    return cfg
