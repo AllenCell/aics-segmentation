b5bfb154fc18a5e014a1fe6e3e8d38928385bff7
unknown <yrkim98@uw.edu>
2021-02-26 15:27:51 -0800

    basic workflow step and engine classes setup

--- aicssegmentation/structure_wrapper/WorkflowEngine.py
+++ aicssegmentation/structure_wrapper/WorkflowEngine.py
@@ -1,21 +1,31 @@
 class WorkflowEngine:
-    def __init__(self, workflow_name):
+    def __init__(self, workflow_name, image):
         self.workflow_name = workflow_name
         self.steps = list()
         self.currentStep = 0
-        self.image = None
-
-    def load_image(self):
-        return 0
-
-    def get_steps(self):
-        return 0
-
-    def apply_step(self):
-        return 0
-
-    def get_image(self):
-        return self.image
+        self.image = [image]
 
     def get_next_step(self):
         return self.steps[self.currentStep]
+
+    def execute_next(self):
+        # Execute the next step and add result to results list
+        self.image.append(self.execute_step())
+        return self.image[-1]
+
+    def execute_step(self):
+        # Perform current step on last image result
+        result = self.steps[self.currentStep].execute(self.image[-1])
+
+        # Keep track of current step
+        self.currentStep = self.currentStep + 1
+        return result
+
+    def see_result(self, step_index):
+        # see the result of a step that has been run
+        if step_index >= self.currentStep:
+            # throw error
+            print("cannot get result at this step, has not been run")
+        else:
+            return self.image[step_index]
+
--- aicssegmentation/structure_wrapper/WorkflowStep.py
+++ aicssegmentation/structure_wrapper/WorkflowStep.py
@@ -1,9 +1,14 @@
 class WorkflowStep:
     def __init__(self, step_config):
         self.name = step_config["name"]
-        self.module = step_config["module"]
-        self.function = step_config["function"]
+        self.function = getattr(step_config["module"], step_config["function"])
         self.parameters = None
         if "parameter" in step_config:
             self.parameters = step_config["parameter"]
         self.parent = step_config["parent"]
+
+    def execute(self, image):
+        if self.parameters:
+            return self.function(image, **self.parameters)
+        else:
+            return self.function(image)
