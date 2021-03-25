77ee78f1d4e979f22c6f0bfed0d7f19421dca1a9
unknown <yrkim98@uw.edu>
2021-03-25 11:06:58 -0700

    touch up comments, fix linting

--- aicssegmentation/structure_wrapper/sample_data.py
+++ aicssegmentation/structure_wrapper/sample_data.py
@@ -5,31 +5,33 @@ import types
 
 __all__ = []
 
+
 class SampleData:
     """
     A class that defines functions that grab sample data from the AICS-segmentation package
     """
 
     def __init__(self):
-        self.all_files = list()
-        self.load_all_image(
-            os.path.join(
-                os.path.split(os.path.dirname(__file__))[0], "..", "demo_data"
-            )
-        )
+        """
+        Constructor for the SampleData class
 
+        Params:
+        """
+        self.all_func = list()  # List of available functions to call
+        self.load_all_image(
+            os.path.join(os.path.split(os.path.dirname(__file__))[0], "..", "demo_data")
+        )
 
     def _load(self, f):
         """
-
-        Args:
+        Load an image from the data demo folder
+        Params:
             f: string
                 file name
 
         Returns:
             img: ndarray
                 Image that has been loaded
-
         """
 
         return np.squeeze(
@@ -41,16 +43,54 @@ class SampleData:
         )
 
     def rab5_demo_data(self):
+        """
+        Test function, load rab5_demo_data and provide and ndarray.
+        Params:
+            f: string
+                file name
+
+        Returns:
+            img: ndarray
+                Image that has been loaded
+        """
         return self._load("RAB5_demo_data.tif")
 
     def make_load_image(self, filename):
-        no_ext = filename.split('.')[0]
-        func_to_run = "def " + no_ext + "(self):" + "\n" + "   return self._load(\"" + filename + "\")"
+        """
+        Function generator to expose all images as python functions.
+        call the generated function for the image by using
+        imagename()
+        Params:
+            filename: string
+                full filename of image in sample_data folder to expose
+
+        Returns:
+            none
+        """
+        no_ext = filename.split(".")[0]
+        func_to_run = (
+            "def "
+            + no_ext
+            + "(self):"
+            + "\n"
+            + '   return self._load("'
+            + filename
+            + '")'
+        )
         exec(func_to_run)
         exec("self." + no_ext + " = types.MethodType(" + no_ext + ", self)")
 
     def load_all_image(self, path):
+        """
+        Expose all images in the path as python functions for the SampleData class
+        Params:
+            path: string
+                path
+
+        Returns:
+            none
+        """
         for filename in os.listdir(path):
-            no_ext = filename.split('.')[0]
+            no_ext = filename.split(".")[0]
             setattr(SampleData, no_ext, self.make_load_image(filename))
             self.all_files.append(no_ext)
