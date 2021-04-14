a0f17ece9e18aa1e82aa5260de622fa28d554bc1
Sylvain Slaton <sylvain.slaton@.alleninstitute.org>
2021-04-14 14:12:06 -0700

    tests

--- aicssegmentation/tests/workflow/test_structure_wrapper_config.py
+++ aicssegmentation/tests/workflow/test_structure_wrapper_config.py
@@ -2,24 +2,57 @@ import pytest
 
 from aicssegmentation.workflow.structure_wrapper_config import StructureWrapperConfig
 
+ALL_STRUCTURE_NAMES = [
+    "actb",
+    "actn1",
+    "atp2a2",
+    "cetn2",
+    "ctnnb1",
+    "dsp",
+    "fbl",
+    "gja1",
+    "h2b_interphase",
+    "lamp1",
+    "lmnb1_interphase",
+    "lmnb1_mitotic",
+    "myh10",
+    "npm1",        
+    "nup153",
+    "pxn",
+    "rab5a",
+    "sec61b",    
+    "slc25a17",
+    "smc1a",
+    "son",
+    "st6gal1",
+    "tjp1",
+    "tomm20",
+    "tuba1b"    
+]
+
 class TestStructureWrapperConfig:
+
+    def test_functions_json_mapping(self):
+        # TODO
+        pass
+
+    def test_workflow_definition_json_mapping(self):
+        # TODO
+        pass
 
     def test_get_all_functions(self):
         functions = StructureWrapperConfig.get_all_functions()
 
         assert functions is not None
-        assert len(functions) > 0
-        # TODO test for real
-
+        assert len(functions) > 0        
 
     @pytest.mark.parametrize("name", [None, "", "  "])    
     def test_get_workflow_definition_empty_name_fails(self, name):
         with pytest.raises(ValueError):
             workflow_def = StructureWrapperConfig.get_workflow_definition(name)        
 
-    def test_get_workflow_definition(self):
-        workflow_def = StructureWrapperConfig.get_workflow_definition("sec61b")
+    @pytest.mark.parametrize("name", ALL_STRUCTURE_NAMES)
+    def test_get_workflow_definition(self, name):
+        workflow_def = StructureWrapperConfig.get_workflow_definition(name)
         assert workflow_def is not None
-
-    def test_all_workflows_definitions(self):
-        pass
+        assert workflow_def.name == name
