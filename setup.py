10b5e847e1a43c9fe09ec872b550605e939afffe
Jianxu Chen <jxchen.ustc@gmail.com>
2021-04-29 21:37:09 -0700

    Bump version: 0.1.22 → 0.2.0

--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     url="https://github.com/AllenCell/aicssegmentation",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.22",
+    version="0.2.0",
     zip_safe=False,
 )
