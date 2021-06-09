fc0d227ae98c89ac73a601f78a03df1fbd0dc92d
Sylvain Slaton <sylvain.slaton@alleninstitute.org>
2021-06-09 11:06:35 -0700

    remove test file

--- aicssegmentation/test.py
+++
@@ -1,8 +0,0 @@
-from aicssegmentation.workflow import WorkflowEngine
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-if __name__ == "__main__":
-    w = WorkflowEngine().get_executable_batch_workflow("sec61b", "/Users/sylvain.slaton/segmenter/input/", "/Users/sylvain.slaton/segmenter/output/")
-    w.execute_all()
