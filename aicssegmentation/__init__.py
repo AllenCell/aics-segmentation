d65fc4d461673390c989443a1d06386740c22460
yrkim98 <yrkim98@uw.edu>
2021-09-14 17:02:10 -0700

    Bump version: 0.4.0 → 0.4.1

--- aicssegmentation/__init__.py
+++ aicssegmentation/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jianxu Chen"
 __email__ = "jianxuc@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 def get_module_version():
