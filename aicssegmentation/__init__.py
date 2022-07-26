b6c42f74686eb214c898de0b185a47a075818f25
Brian Kim <brian.kim@alleninstitute.org>
2022-07-26 14:02:02 -0700

    Bump version: 0.5.1 → 0.5.2

--- aicssegmentation/__init__.py
+++ aicssegmentation/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jianxu Chen"
 __email__ = "jianxuc@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 
 def get_module_version():
